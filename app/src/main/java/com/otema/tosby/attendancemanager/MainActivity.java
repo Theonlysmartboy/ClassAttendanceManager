4406a26eab6fc838c628ece9a8cd22bd30450e00
Tosby <o2jose43@gmail.com>
2017-05-08 15:47:58 +0300

    Added more activity files

--- app/src/main/java/com/otema/tosby/attendancemanager/MainActivity.java
+++ app/src/main/java/com/otema/tosby/attendancemanager/MainActivity.java
@@ -2,18 +2,22 @@ package com.otema.tosby.attendancemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
-
-import com.otema.tosby.attendancemanager.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 TextView greet;
+    Button Timetable,Profile,Attendance;
     LoginActivity loginActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Button refferences
+        Timetable= (Button)findViewById(R.id.Timetable_btn);
+        Profile=(Button)findViewById(R.id.prophile_button);
+        Attendance=(Button)findViewById(R.id.attendance_btn);
+        
           greet = (TextView)findViewById(R.id.greeting);
         loginActivity = new LoginActivity();
         //greet.setText(UserName);
