5c37515b1a917806b05914e1861ab0771ba20f7c
Tosby <o2jose43@gmail.com>
2017-06-07 17:37:13 +0300

    Finished the fingerprint authentification

--- app/src/main/java/com/otema/tosby/attendancemanager/MainActivity.java
+++ app/src/main/java/com/otema/tosby/attendancemanager/MainActivity.java
@@ -1,13 +1,14 @@
 package com.otema.tosby.attendancemanager;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 //TextView greet;
     Button Timetable,Profile,Attendance;
     LoginActivity loginActivity;
@@ -19,21 +20,31 @@ public class MainActivity extends AppCompatActivity {
         Timetable= (Button)findViewById(R.id.Timetable_btn);
         Profile=(Button)findViewById(R.id.prophile_button);
         Attendance=(Button)findViewById(R.id.attendance_btn);
-
+        Timetable.setOnClickListener(this);
+        Profile.setOnClickListener(this);
+        Attendance.setOnClickListener(this);
 
     }
-    public void ViewTimetable(View V){
-final Dialog Timetabledialog = new Dialog(MainActivity.this);
-        Timetabledialog.setContentView(R.layout.activity_timetable);
-        Timetabledialog.setTitle("Timetable");
-        Timetabledialog.setCancelable(true);
-        Timetabledialog.show();
-    }
-    public void SubmitAttendance(View V){
-        final Dialog Attendancedialog = new Dialog(MainActivity.this);
-        Attendancedialog.setContentView(R.layout.activity_attendance);
-        Attendancedialog.setTitle("Submit Attendance");
-        Attendancedialog.setCancelable(true);
-        Attendancedialog.show();
+
+    @Override
+    public void onClick(View view) {
+switch (view.getId()){
+    case R.id.Timetable_btn:
+        Intent timetable = new Intent(getApplicationContext(),Timetable.class);
+        startActivity(timetable);
+        finish();
+        break;
+    case R.id.prophile_button:
+        Intent prophile = new Intent(getApplicationContext(),MyProfile.class);
+        startActivity(prophile);
+        finish();
+        break;
+    case R.id.attendance_btn:
+        Intent attendance = new Intent(getApplicationContext(),Attendance.class);
+        startActivity(attendance);
+        finish();
+        break;
+    default:
+}
     }
 }
